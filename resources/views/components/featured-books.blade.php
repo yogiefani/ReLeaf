0df70c1ff606a99253847d0dd57d668215973727
yogiefani <yogiefaniyc@gmail.com>
2025-07-08 20:31:21 +0700

    feat: add featured book

---
+++ resources/views/components/featured-books.blade.php
@@ -0,0 +1,17 @@
+<div class="container mx-auto px-6 md:px-12 py-16 md:py-24 bg-white">
+    <div class="flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
+        <div class="md:w-1/2 mt-12 md:mt-0 flex justify-center md:justify-end">
+            <img src="{{ asset('images/book.png') }}" alt="Ilustrasi membaca buku" class="max-w-md w-full">
+        </div>
+        <div class="md:w-1/2 mt-0 md:mt-28 px-10 text-center md:text-left">
+            <h1 class="font-playfair font-regular text-5xl md:text-7xl leading-tight text-black">
+                Temukan Buku Favoritmu Disini
+            </h1>
+            <p class="font-oxygen mt-6 text-lg text-gray-700">
+                Dengan buku fisik, Anda dapat merasakan pengalaman membaca yang lebih nyata dan mendalam. Nikmati aroma khas kertas, sensasi membalik halaman, dan fokus penuh tanpa gangguan layar. Bawa buku favorit Anda ke taman, kafe, atau ruang santai, dan rasakan kenikmatan membaca secara langsung di setiap tempat.
+            </p>
+        </div>
+
+
+    </div>
+</div>
